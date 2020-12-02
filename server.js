05f5fa2995efe79a416e18f76692d6b9b635d997
Tyler Linen <66691586+tylerlinen@users.noreply.github.com>
2020-12-01 23:50:52 -0500

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Budget_Tracker',
+  {
   useNewUrlParser: true,
   useFindAndModify: false
 });
